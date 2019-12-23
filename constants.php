661647d2b9381775c20d0652fcb11895f208605f
satyabratapanigrahi <57784978+satyabratapanigrahi@users.noreply.github.com>
2019-12-23 20:31:58 +0530

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+define("HOST","localhost");
+define("USER","root");
+define("PASS","");
+define("DB","project_inv");
+
+define("DOMAIN","http://localhost:8181/project_inv
+	");
+
+?>
+
